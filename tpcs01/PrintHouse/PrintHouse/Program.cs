614cd18e1b4725ef37c4952a2119be88886ad8d2
Di Lam Nguyen <di_lam.nguyen@localhost.localdomain>
2016-11-14 16:58:07 +0100

    tpcs01

--- tpcs01/PrintHouse/PrintHouse/Program.cs
+++ tpcs01/PrintHouse/PrintHouse/Program.cs
@@ -6,7 +6,40 @@ namespace PrintHouse
 	{
 		public static void Main (string[] args)
 		{
-			PrintHouse (10);
+			Console.Write ("Do you want to see the simple house (1) or the bonii (2)? ");
+			string i = Console.ReadLine();
+			if (i == "1")
+				PrintHouse ();
+			else if (i == "2")
+				PrintHouse_ (10);
+			else
+				Console.WriteLine("Invalid response.");
+		}
+
+		public static void Roof()
+		{
+			Console.WriteLine ("  ___________");
+			Console.WriteLine (" / | |   | | \\");
+			Console.WriteLine ("---------------");
+		}
+
+		public static void Floor()
+		{
+			Console.WriteLine ("|  |0|   |0|  |");
+			Console.WriteLine ("|             |");
+		}
+
+		public static void Ground()
+		{
+			Console.WriteLine ("|      _      |");
+			Console.WriteLine ("|_____|-|_____|");
+		}
+
+		public static void PrintHouse()
+		{
+			Roof ();
+			Floor ();
+			Ground ();
 		}
 
 		public static void Smoke(int x)
@@ -21,7 +54,7 @@ namespace PrintHouse
 			Console.WriteLine ();
 		}
 
-		public static void Roof(int x)
+		public static void Roof_(int x)
 		{
 			if (x < 0)
 				Console.Write ("       ");
@@ -33,35 +66,35 @@ namespace PrintHouse
 			Console.WriteLine ("        /\\\n   =====      /  \\\n  _|___|_____/ __ \\____________\n |::::::::::/ |  | \\:::::::::::|\n |:::::::::/  ====  \\::::::::::|\n |::::::::/__________\\:::::::::|\n |_________|  ____  |__________|");
 		}
 
-		public static void Floor()
+		public static void Floor_()
 		{
 			Console.WriteLine ("  | ______ | / || \\ | _______ |\n  ||  |   || ====== ||   |   ||\n  ||--+---|| |    | ||---+---||\n  ||__|___|| |   o| ||___|___||\n  |========| |____| |=========|");
 		}
 
-		public static void Ground()
+		public static void Ground_()
 		{
 			Console.WriteLine (" (^^-^^^^^-|________|-^^^--^^^)\n (,, , ,, ,/________\\,,,, ,, ,)\n','',,,,' /__________\\,,,',',;;");
 		}
 
-		public static void PrintHouse(int x)
+		public static void PrintHouse_(int x)
 		{
 			if (x < 0) 
 			{
 				Console.Clear ();
 				Smoke (x);
-				Roof (x);
-				Floor ();
-				Ground ();
+				Roof_ (x);
+				Floor_ ();
+				Ground_ ();
 			}
 			else 
 			{
 				Console.Clear ();
 				Smoke (x);
-				Roof (x);
-				Floor ();
-				Ground ();
+				Roof_ (x);
+				Floor_ ();
+				Ground_ ();
 				System.Threading.Thread.Sleep(1000);
-				PrintHouse (x - 1);
+				PrintHouse_ (x - 1);
 			}
 		}
 	}
